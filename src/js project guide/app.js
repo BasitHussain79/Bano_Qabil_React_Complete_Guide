04c93ad06a835facf4ea1c321ca08bbcf92fdc21
BasitHussain79 <basithussainqadri@gmail.com>
2023-06-11 21:45:11 +0500

    feat: form ui complete

--- src/js project guide/app.js
+++ src/js project guide/app.js
@@ -4,6 +4,8 @@ const modifyArr = arr.map((d) => {
   return d * 2;
 });
 
+console.log(modifyArr);
+
 const studArr = [
   { name: "name1", fees: 4000 },
   { name: "name2", fees: 5000 },
@@ -12,9 +14,13 @@ const studArr = [
 ];
 
 const discountFees = studArr.map((stud) => {
+  // if (stud.fees > 2000) {
+  //   return { name: stud.name, fees: stud.fees / 2 };
+  // } else {
+  //   return stud;
+  // }
+
   return stud.fees > 2000 ? { name: stud.name, fees: stud.fees / 2 } : stud;
 });
 
 console.log(discountFees);
-
-console.log(modifyArr);
