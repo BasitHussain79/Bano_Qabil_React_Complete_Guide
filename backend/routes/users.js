bcb315d21cb0a6d9abc2feead224872aee89a598
BasitHussain79 <basithussainqadri@gmail.com>
2023-07-15 21:07:40 +0500

    feat: auth and register user completed

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const { check, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
 const Users = require("../model/User");
 const router = express.Router();
 
@@ -36,8 +39,8 @@ router.post(
     try {
       let user = await Users.findOne({ email });
       if (user) {
-        return res.status(401).json({
-          status: 401,
+        return res.status(400).json({
+          status: 400,
           msg: "User with this email already exists.",
         });
       }
@@ -52,10 +55,24 @@ router.post(
       user.password = await bcrypt.hash(password, salt);
 
       await user.save();
-      res.status(200).json({
-        status: 200,
-        msg: "User is created successfully",
-      });
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 3600000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.log(err.message);
       res.status(500).json({
